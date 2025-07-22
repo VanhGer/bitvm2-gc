6f861df0aa2b4d7c76b1ab0ffb36d3ec2e04df54
vanhger <vietanhpg2003@gmail.com>
2025-07-22 14:27:52 +0700

    feat: add oom example

--- verifiable-circuit-host/examples/gen-sub-circuits.rs
+++ verifiable-circuit-host/examples/gen-sub-circuits.rs
@@ -1,21 +1,28 @@
 //! cargo run -r --example gen-sub-circuits -- --nocapture
-use garbled_snark_verifier::circuits::bigint::{
-    U254,
-    utils::{biguint_from_wires, random_biguint_n_bits},
-};
-use garbled_snark_verifier::circuits::bn254::fp254impl::Fp254Impl;
-use garbled_snark_verifier::circuits::bn254::fq::Fq;
+
+use garbled_snark_verifier::bag::{new_wirex, Wires, Wirex};
+use garbled_snark_verifier::circuits::bigint::add::add_generic;
+use rand;
+
+fn new_wirex_with_random_value() -> Wirex {
+    let mut wire = new_wirex();
+    wire.borrow_mut().set(rand::random::<bool>());
+    wire
+}
+
+fn n_random_wires(n: usize) -> Wires {
+    (0..n).map(|_| new_wirex_with_random_value()).collect()
+}
 
 fn main() {
-    let a = Fq::random();
-    let mut circuit = Fq::div6(Fq::wires_set(a));
+    let a_wires = n_random_wires(1747627);
+    let b_wires = n_random_wires(1747627);
+
+    let mut circuit = add_generic(a_wires, b_wires, 1747627);
     circuit.gate_counts().print();
-    for gate in &mut circuit.1 {
+    for mut gate in &mut circuit.1 {
         gate.evaluate();
     }
-
-    let c = Fq::from_wires(circuit.0.clone());
-    assert_eq!(c + c + c + c + c + c, a);
 
     let garbled = circuit.garbled_gates();
 
--- verifiable-circuit/src/main.rs
+++ verifiable-circuit/src/main.rs
@@ -9,23 +9,32 @@
 #![no_main]
 zkm_zkvm::entrypoint!(main);
 
-use garbled_snark_verifier::circuits::bigint::{
-    utils::{biguint_from_wires, random_biguint_n_bits},
-    U254,
-};
 use garbled_snark_verifier::circuits::bn254::fp254impl::Fp254Impl;
 use garbled_snark_verifier::circuits::bn254::fq::Fq;
 
-fn main() {
-    let a = Fq::random();
-    let mut circuit = Fq::div6(Fq::wires_set(a));
-    circuit.gate_counts().print();
-    //for mut gate in circuit.1 {
-    //    gate.evaluate();
-    //}
+use garbled_snark_verifier::bag::{new_wirex, Wires, Wirex};
+use garbled_snark_verifier::circuits::bigint::add::add_generic;
+use rand;
 
-    //let c = Fq::from_wires(circuit.0);
-    //assert_eq!(c + c + c + c + c + c, a);
+fn new_wirex_with_random_value() -> Wirex {
+    let mut wire = new_wirex();
+    wire.borrow_mut().set(rand::random::<bool>());
+    wire
+}
+
+fn n_random_wires(n: usize) -> Wires {
+    (0..n).map(|_| new_wirex_with_random_value()).collect()
+}
+
+fn main() {
+    let a_wires = n_random_wires(1747627);
+    let b_wires = n_random_wires(1747627);
+
+    let mut circuit = add_generic(a_wires, b_wires, 1747627);
+    circuit.gate_counts().print();
+    for gate in &mut circuit.1 {
+        gate.evaluate();
+    }
 
     let garbled = circuit.garbled_gates();
     zkm_zkvm::io::commit(&garbled.len());
