1225a243612ac3cdecc3977a04a1645fa9a4a44c
vanhger <vietanhpg2003@gmail.com>
2026-05-08 11:43:27 +0700

    update the witness size.

--- verifiable-circuit-babe/tests/babe_e2e.rs
+++ verifiable-circuit-babe/tests/babe_e2e.rs
@@ -4,7 +4,7 @@ use verifiable_circuit_babe::babe::run_babe_e2e_cac;
 fn e2e_babe_cac() {
     use verifiable_circuit_babe::transactions::OnchainSize;
     let run = run_babe_e2e_cac();
-    assert_eq!(run.assert_witness.size_bytes(), 12192); // 762 * 16
-    assert_eq!(run.challenge_assert_witness.size_bytes(), 24448); // 762 * 16 * 2 + 64
+    assert_eq!(run.assert_witness.size_bytes(), 1960); // 98 * 20
+    assert_eq!(run.challenge_assert_witness.size_bytes(), 14312);
     assert_eq!(run.wrongly_challenged_witness.size_bytes(), 64);
 }
